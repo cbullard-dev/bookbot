c142bdb858009b4767dfa7ea5b1a824aae545b02
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-12-16 16:49:35 +0900

    Updated the stats file with a new function to collect the stats from the book and sort them Updated the main file with formatting and comments to section out the main function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from stats import count_words
+from stats import count_words, count_characters, sort_counts
+
+TITLE = "============ BOOKBOT ============"
+WORD_COUNT_HEADER = "----------- Word Count ----------"
+CHARACTER_COUNT_HEADER = "--------- Character Count -------"
+END = "============= END ==============="
+
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -6,9 +12,28 @@ def get_book_text(filepath):
   return file_contents
 
 def main():
-  path = "./books/frankenstein.txt"
+  path = "books/frankenstein.txt"
+
+  # Complete the collections and calculations
   book = get_book_text(path)
   wordCount = count_words(book)
+  chars = count_characters(book)
+  sortCount = sort_counts(chars)
+
+  # Print the message body
+  print(TITLE)
+  print(f"Analyzing book found at {path}...")
+  print(WORD_COUNT_HEADER)
   print(f"Found {wordCount} total words")
+  print(CHARACTER_COUNT_HEADER)
+  for item in sortCount:
+    char = item["name"]
+    if not char.isalpha():
+      continue
+    count = item["num"]
+    print(f"{char}: {count}")
+
+  print(END)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,25 @@
 def count_words(book):
   words = book.split()
   return len(words)
+
+def count_characters(book):
+  lower_book = book.lower()
+  chars = {}
+  for char in lower_book:
+    if char not in chars:
+      chars[char] = 1
+      continue
+    chars[char] += 1
+  return chars
+
+def sort_counts(count_dictionary):
+  count_list = []
+  for key in count_dictionary:
+    element = {"name": key,
+              "num": count_dictionary[key]}
+    count_list.append(element)
+  count_list.sort(reverse=True, key=sort_on)
+  return count_list
+
+def sort_on(items):
+  return items["num"]
